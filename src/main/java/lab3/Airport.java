41b3de85afbbb1b11494073909516b174c77238d
Parviz <alievz2011@gmail.com>
2021-12-17 22:50:24 +0300

    Scripted auto-commit on change (2021-12-17 22:50:24) by gitwatch.sh

--- src/main/java/lab3/Airport.java
+++ src/main/java/lab3/Airport.java
@@ -3,6 +3,8 @@ package lab3;
 import java.io.Serializable;
 
 public class Airport implements Serializable {
+    private final String name;
+
     public String toString() {
         return String.format("Airport{id=%d, name='%s'}", id, name);
 }
