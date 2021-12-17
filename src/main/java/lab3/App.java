f4b124ed324f4954c6a6f7bf49fed36b449b703c
Parviz <alievz2011@gmail.com>
2021-12-17 22:38:49 +0300

    Scripted auto-commit on change (2021-12-17 22:38:49) by gitwatch.sh

---
+++ src/main/java/lab3/App.java
@@ -0,0 +1,11 @@
+package lab3;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+import scala.Tuple2;
+
+import java.util.Map;
+
+public class App {
--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -3,6 +3,8 @@ package lab3;
 import org.apache.spark.SparkConf;
 
 public class Main {
+    private static final String APP_NAME = "Lab_03";
+
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
         new App(conf).run();
