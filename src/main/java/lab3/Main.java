a008d8acd47bd64c7440f94ec2984d66a6a190ae
Parviz <alievz2011@gmail.com>
2021-12-17 22:38:20 +0300

    Scripted auto-commit on change (2021-12-17 22:38:20) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -4,4 +4,6 @@ import org.apache.spark.SparkConf;
 
 public class Main {
     public static void main(String[] args){
+        SparkConf conf = new SparkConf().setAppName(APP_NAME);
+        new App(conf).run();
 }
