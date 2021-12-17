98e839914cf1c992f8316ce4ad93104479d5893a
Parviz <alievz2011@gmail.com>
2021-12-17 22:44:12 +0300

    Scripted auto-commit on change (2021-12-17 22:44:12) by gitwatch.sh

--- src/main/java/lab3/Flight.java
+++ src/main/java/lab3/Flight.java
@@ -1,6 +1,7 @@
 package lab3;
 
 public class Flight {
+    private final float delayTime;
     public Flight(boolean isCancelled, float delayTime, int airportId, int destId) {
         this.isCancelled = isCancelled;
         this.delayTime = delayTime;
